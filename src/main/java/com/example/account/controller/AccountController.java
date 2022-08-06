f547d935c65f3700f12df3eca274bf23467e2fdc
kormeian <ooliskoo@naver.com>
2022-08-07 04:48:49 +0900

    계좌 확인 API-002

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -1,13 +1,17 @@
 package com.example.account.controller;
 
 import com.example.account.domain.Account;
+import com.example.account.dto.AccountInfo;
 import com.example.account.dto.CreateAccount;
+import com.example.account.dto.DeleteAccount;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +20,39 @@ public class AccountController {
     private final RedisTestService redisTestService;
 
     @PostMapping("/account")
-    public String createAccount(
-            @RequestBody @Valid CreateAccount.Request
-            ) {
-        accountService.createAccount();
-        return "success";
+    public CreateAccount.Response createAccount(
+            @RequestBody @Valid CreateAccount.Request request
+    ) {
+        return CreateAccount.Response.from(
+                accountService.createAccount(
+                        request.getUserId(),
+                        request.getInitialBalance()));
+    }
+
+    @DeleteMapping("/account")
+    public DeleteAccount.Response deleteAccount(
+            @RequestBody @Valid DeleteAccount.Request request
+    ) {
+
+        return DeleteAccount.Response.from(
+                accountService.deleteAccount(
+                        request.getUserId(),
+                        request.getAccountNumber()
+                )
+        );
+    }
+
+    @GetMapping("/account")
+    public List<AccountInfo> getAccountByUserId(
+            @RequestParam("user_id") Long userId
+    ) {
+        return accountService.getAccountsByUserId(userId)
+                .stream().map(accountDto ->
+                        AccountInfo.builder()
+                                .accountNumber(accountDto.getAccountNumber())
+                                .balance(accountDto.getBalance())
+                                .build())
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/get-lock")
@@ -30,7 +62,7 @@ public class AccountController {
 
     @GetMapping("/account/{id}")
     public Account getAccount(
-            @PathVariable Long id){
+            @PathVariable Long id) {
         return accountService.getAccount(id);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT into account_user(id, name, created_at, updated_at)
+VALUES(1, 'Pororo', now(), now());
+INSERT into account_user(id, name, created_at, updated_at)
+VALUES(2, 'Lupi', now(), now());
+INSERT into account_user(id, name, created_at, updated_at)
+VALUES(3, 'Eddie', now(), now());
